f68ca23c9a81e06de204c65db742e309f61041e4
Michael Giebler <megiebler@mail.fhsu.edu>
2023-02-02 22:55:21 -0600

    first commit

--- index.php
+++ index.php
@@ -4,33 +4,34 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" tyep="text/css" href="main.css">
+    <title>Assignment 1</title>
 </head>
 <body>
-<?php
-//fetching $firstname from the browser's url directly
-$firstname=filter_input(INPUT_GET, 'firstname', FILTER_SANITIZE_SPECIAL_CHARS);
-//fetching $lastname from the browser's url directly
-$lastname=filter_input(INPUT_GET, 'lastname', FILTER_SANITIZE_SPECIAL_CHARS);
-//fetching $age from the browser's url directly
-$age=filter_input(INPUT_GET, 'age', FILTER_SANITIZE_NUMBER_INT);
-//displaying the string 
-echo "Hello, my name is $firstname $lastname.<br/>";
-//based on condition, displaying the string
-if($age>=18)
-{
-echo "I am $age years old and I am old enough to vote in the United States.<br/>";
-}
-else
-{
-echo "I am $age years old and I am not old enough to vote in the United States.<br/>";
-}
-//calculating number of days from age
-$numofdays = $age  * 365;
-echo "Number of days from the age is $numofdays <br/>";
-//displaying current date
-$date = date('Y-m-d H:i:s');
-echo $date;
-?>
+    <?php
+    //retrieve first name from url 
+    $firstname=filter_input(INPUT_GET, 'firstname', FILTER_SANITIZE_SPECIAL_CHARS);
+    //retrieve last name from url
+    $lastname=filter_input(INPUT_GET, 'lastname', FILTER_SANITIZE_SPECIAL_CHARS);
+    //retrieve age from url
+    $age=filter_input(INPUT_GET, 'age', FILTER_SANITIZE_NUMBER_INT);
+    //requirement 1: display first and last name
+    echo "Hello, my name is $firstname $lastname.<br/>";
+    //if the age is greater than or equal to 18 relay that they can vote
+    if($age>=18)
+    {
+    echo "I am $age years old and I am old enough to vote in the United States.<br/>";
+    }
+    else //if age is not greater or equal to 18 then relay that they cannot vote 
+    {
+    echo "I am $age years old and I am not old enough to vote in the United States.<br/>";
+    }
+    //calculate number of days from age
+    $numofdays = $age  * 365;
+    echo "Number of days from the age is $numofdays <br/>";
+    //displays current date
+    $date = date('Y-m-d H:i:s');
+    echo $date;
+    ?>
 </body>
 </html>
